7fe1eb4eec89d4ba0bbcfdb471ab1ace1d2e4071
Anthony Chu <anthony@anthonychu.ca>
2021-04-07 22:53:26 -0700

    Add api

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ time }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      time: null
+    }
+  },
+  async mounted() {
+    const response = await fetch("/api/info")
+    this.time = await response.text()
+  }
+}
+</script>
