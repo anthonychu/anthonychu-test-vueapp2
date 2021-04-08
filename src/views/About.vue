b39075393a12934662fce24e66115ea1f38a2acb
Anthony Chu <anthony@anthonychu.ca>
2021-04-07 23:31:42 -0700

    Add config

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
     <h1>{{ time }}</h1>
+
+    <div v-if="user">
+      {{ user.clientPrincipal.userDetails }} | <a href="/.auth/logout">Logout</a>
+    </div>
+    <div v-else>
+      <a href="/.auth/login/github">Login</a>
+    </div>
   </div>
 </template>
 
@@ -8,12 +15,18 @@
 export default {
   data() {
     return {
-      time: null
+      time: null,
+      user: null
     }
   },
   async mounted() {
     const response = await fetch("/api/info")
     this.time = await response.text()
+
+    const userResponse = await fetch("/.auth/me")
+    if (userResponse.status < 300) {
+      this.user = await userResponse.json()
+    }
   }
 }
 </script>
